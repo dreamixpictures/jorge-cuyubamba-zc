49a98ac4be1d47984a7011180154992421424c9e
Jorge Cuyubamba <yostin.derefe@gmail.com>
2020-07-14 13:45:46 -0500

    fork and mail

---
+++ README.md
@@ -0,0 +1,3 @@
+![GitHub repo size](https://img.shields.io/github/repo-size/jorge-cuyubamba/jorge-cuyubamba-zc?style=for-the-badge)
+
+https://jorge-cuyubamba.github.io/jorge-cuyubamba-zc/
---
+++ zjchem/st/chem/js/alter.js
@@ -0,0 +1,1 @@
+function showInfoDialog(a){a="\x3cdiv class\x3d'pop-l-pc'\x3e\x3cdiv class\x3d'pop-s-pc'\x3e\x3cp\x3e"+a+"\x3c/p\x3e";a+="\x3cbutton  onclick\x3d'showDialog()'\x3e\u77e5\u9053\u4e86\x3c/button\x3e";a+="\x3c/div\x3e";a+="\x3c/div\x3e";$(".wrapper").after(a)}function showDialog(){$(".pop-l-pc").remove()};
